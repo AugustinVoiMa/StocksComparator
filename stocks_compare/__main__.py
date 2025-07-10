f92824279ebba60afac9cd3a44544c062313c8ab
Augustin Voisin-Marras <augustin.voisin-marras@csgroup.eu>
2025-07-10 15:30:41 +0200

    ready to publish

--- stocks_compare/__main__.py
+++ stocks_compare/__main__.py
@@ -1,10 +1,26 @@
-from functools import partial
+from enum import Enum
 
 import pandas as pd
 import streamlit as st
 import yfinance as yf
 
-from stocks_compare.stocks_data import retrieve_data
+
+class ScaleEnum(Enum):
+    SCALE_1Y = "1y"
+
+
+def retrieve_data(symbol, scale: ScaleEnum = ScaleEnum.SCALE_1Y):
+    data = yf.download(symbol, start="2013-01-01", end="2024-12-31")
+
+    if scale == ScaleEnum.SCALE_1Y:
+        data["Year"] = data.index.year
+        close_col = "Adj Close" if "Adj Close" in data.columns else "Close"
+        data = data.groupby("Year").last("Adj Close")[(close_col, symbol)]
+        data.name = symbol
+    else:
+        raise Exception("Not Implemented")
+    return data
+
 
 st.title("Stocks comparator")
 
@@ -29,6 +45,7 @@ def select_stock(stock_id):
             st.session_state[stock_id] = {'symbol': selected, 'name': result.quotes[idx]["shortname"]}
             st.rerun()
 
+
 data1 = data2 = None
 
 if st.session_state.get("stock1") is not None:
@@ -37,16 +54,11 @@ if st.session_state.get("stock1") is not None:
 if st.button("Select Stock 1"):
     select_stock("stock1")
 
-
-
-
 if st.session_state.get("stock2") is not None:
     st.write(f"Stock 2: {st.session_state.stock2['name']} ({st.session_state.stock2['symbol']})")
     data2 = retrieve_data(st.session_state.stock2['symbol'])
 if st.button("Select Stock 2"):
     select_stock("stock2")
-
-
 
 all_data = [d for d in [data1, data2] if d is not None]
 
